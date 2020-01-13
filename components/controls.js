659ae7a692a12d4aeeb7f113c271608095df1af8
thatartplace <49554510+thatartplace@users.noreply.github.com>
2020-01-12 21:19:31 -0500

    added playback buttons & restructure playback controls

---
+++ components/controls.js
@@ -0,0 +1,81 @@
+import React, {useContext} from "react";
+import {VisualizerStates} from "./contexts";
+
+const Space = () => <Group grow> </Group>;
+
+const Group = ({children, grow}) => (
+    <div>
+        {children}
+        <style jsx>{`
+          div {
+            display: inline-flex;
+            flex-grow: ${grow ? 1 : 0};
+          }
+        `}</style>
+    </div>
+);
+
+const Slider = ({onChange, min, max, init, title = null}) => (
+    <Group grow>
+        {title && <span>{title}</span>}
+        <input type="range" min={min} max={max} defaultValue={init} onChange={e => onChange(e.target.value)}/>
+        <style jsx>{`
+          input[type="range"] {
+            appearance: none;
+            background: #CD9841;
+            border-radius: 5px;
+            margin: 0 5px;
+            flex-grow: inherit;
+          }
+          span {
+            margin-left: 5px;
+            color: lightgrey;
+          }
+        `}</style>
+    </Group>
+);
+
+const Button = ({children}) => (
+    <>
+        <button>{children}</button>
+        <style jsx>{`
+          button {
+            background: none;
+            border: 2px solid white;
+            border-radius: 10px;
+            color: white;
+            height: 30px;
+            margin: 0 5px;
+          }
+        `}</style>
+    </>
+);
+
+export default () => {
+    const {
+        N: [N, set_N],
+        step: [step, set_step]
+    } = useContext(VisualizerStates);
+
+    return (
+        <div>
+            <Group>
+                <Button>Back</Button>
+                <Button>Pause</Button>
+                <Button>Forward</Button>
+            </Group>
+            <Space/>
+            <Slider onChange={set_N} title="Input size" min="1" max="500" init={N}/>
+            <Space/>
+            <Button>Shuffle</Button>
+            <style jsx>{`
+              div {
+                color: darkgrey;
+                display: flex;
+                align-items: center;
+                justify-content: space-evenly;
+              }
+            `}</style>
+        </div>
+    );
+};
--- pages/[visualizer].js
+++ pages/[visualizer].js
@@ -1,67 +1,33 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
 import {useRouter} from "next/router";
 import Nav from "../components/nav";
-
-const Slider = ({onChange, min, max, initial}) => (
-    <>
-        <input type="range" min={min} max={max} defaultValue={initial} onChange={e => onChange(e.target.value)}/>
-        <style jsx>{`
-          input[type="range"] {
-            appearance: none;
-            background: #CD9841;
-            border-radius: 5px;
-            flex-grow: 1;
-          }
-        `}</style>
-    </>
-);
-
-const Input = ({N, set_N}) => (
-    <div className="container">
-        <div className="group grow">
-            <span>Input size</span>
-            <Slider onChange={n => set_N(n)} min="1" max="500" initial={N}/>
-        </div>
-        <div className="group">
-            <button>Shuffle</button>
-        </div>
-        <style jsx>{`
-          div.container {
-            color: darkgrey;
-            padding: 10px;
-            display: flex;
-          }
-          div.group {
-            padding: 10px 20px;
-            display: inline-flex;
-          }
-          div.group > span {
-            margin-right: 5px;
-          }
-          .grow {
-            flex-grow: 1;
-          }
-        `}</style>
-    </div>
-);
+import Controls from "../components/controls";
+import {VisualizerStates} from "../components/contexts";
 
 export default () => {
-    const [N, set_N] = useState(500);
+    const N = useState(100);
+    const paused = useState(false);
+    const step = useState(0);
+    const provideStates = children =>
+        <VisualizerStates.Provider
+            value={{N, paused, step}}
+        >{children}</VisualizerStates.Provider>;
 
-    return (
+    return provideStates(
         <Nav>
             <div className="container">
-                <div className="input"><Input N={N} set_N={set_N}/></div>
+                <div className="controls"><Controls/></div>
                 <style jsx>{`
-              div.container {
-                display: flex;
-              }
-              div.input {
-                width: 100%;
-                margin: 20px;
-                border-bottom: 2px solid grey;
-              }
-            `}</style>
+                  div.container {
+                    display: flex;
+                  }
+                  div.controls {
+                    width: 100%;
+                    margin: 0 20px;
+                    padding: 15px 0;
+                    border-bottom: 2px solid grey;
+                  }
+                `}</style>
             </div>
         </Nav>
     );
